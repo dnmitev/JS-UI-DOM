55cb24f70ed643fd4bf65ecf5cdf23ff1ec53d9f
Dimitar Mitev <dnmitev@gmail.com>
2014-06-02 15:20:27 +0300

    Bouncing Ball is done

--- Canvas/01. JustDrawings/DrawingsGenerator.js
+++ Canvas/01. JustDrawings/DrawingsGenerator.js
@@ -57,25 +57,20 @@
     ctx.beginPath();
     ctx.moveTo(10, 300);
     ctx.lineTo(135, 150);
-    ctx.moveTo(135, 150);
-    ctx.lineTo(180, 200);
-    ctx.moveTo(180, 235);
-    ctx.lineTo(180, 160);
-    ctx.moveTo(200, 235);
-    ctx.lineTo(200, 160);
-    ctx.moveTo(200, 220);
     ctx.lineTo(270, 300);
-    ctx.closePath();
     ctx.fillStyle = "#975B5B";
     ctx.fill();
     ctx.stroke();
 
     // draw the chimney
     ctx.beginPath();
-
-    ctx.closePath()
+    ctx.lineTo(180, 200);
+    ctx.moveTo(180, 235);
+    ctx.lineTo(180, 160);
+    ctx.moveTo(200, 235);
+    ctx.lineTo(200, 160);
+    ctx.fillRect(180, 160, 20, 75);
     ctx.stroke();
-    ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(180, 160);
@@ -85,4 +80,145 @@
     ctx.stroke();
     ctx.fill();
 
+
+
+    // draw the bike
+    // starts with the wheels
+    ctx.beginPath();
+    ctx.arc(400, 500, 25, 0, 2 * Math.PI);
+    ctx.fillStyle = '#90CAD7';
+    ctx.fill();
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(500, 500, 25, 0, 2 * Math.PI);
+    ctx.fillStyle = '#90CAD7';
+    ctx.fill();
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    // draw the frame
+    ctx.beginPath();
+    ctx.moveTo(400, 500);
+    ctx.lineTo(445, 500);
+    ctx.lineTo(490, 465);
+    ctx.lineTo(425, 465);
+    ctx.closePath();
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    // draw the steering system
+    ctx.beginPath();
+    ctx.moveTo(500, 500);
+    ctx.lineTo(485, 440);
+    ctx.lineTo(465, 450);
+    ctx.moveTo(485, 440);
+    ctx.lineTo(500, 420);
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    // draw the seat
+    ctx.beginPath();
+    ctx.moveTo(445, 500);
+    ctx.lineTo(415, 450);
+    ctx.moveTo(405, 450);
+    ctx.lineTo(430, 450);
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    // draw the pedal system
+    ctx.beginPath();
+    ctx.arc(445, 500, 5, 0, 2 * Math.PI);
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(440, 495);
+    ctx.lineTo(435, 490);
+    ctx.moveTo(450, 505);
+    ctx.lineTo(455, 510);
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+
+
+    // draw like a sir
+    // lets start from the head
+    ctx.beginPath();
+    ctx.arc(500, 200, 50, 0, 2 * Math.PI);
+    ctx.fillStyle = '#90CAD7';
+    ctx.fill();
+    ctx.strokeStyle = '#337D8F';
+    ctx.stroke();
+
+    // draw creepy face
+    ctx.save();
+    ctx.scale(1, 0.4);
+    ctx.beginPath();
+    ctx.arc(500, 580, 25, 0, 2 * Math.PI);
+    ctx.restore();
+    ctx.stroke();
+
+    ctx.save();
+    ctx.scale(1, 0.35);
+    ctx.beginPath();
+    ctx.arc(470, 540, 12, 0, 2 * Math.PI);
+    ctx.restore();
+    ctx.stroke();
+
+    ctx.save();
+    ctx.scale(1, 0.35);
+    ctx.beginPath();
+    ctx.arc(530, 540, 12, 0, 2 * Math.PI);
+    ctx.restore();
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(470, 189, 3, 0, 2 * Math.PI);
+    ctx.fillStyle = '#000';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(530, 189, 3, 0, 2 * Math.PI);
+    ctx.fillStyle = '#000';
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(500, 190);
+    ctx.lineTo(485, 210);
+    ctx.lineTo(500, 205);
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+
+    // draw the hat - hopefully there is not a bunny inside
+    ctx.save();
+    ctx.scale(1, 0.3);
+    ctx.beginPath();
+    ctx.arc(500, 520, 60, 0, 2 * Math.PI);
+    ctx.fillStyle = '#396693';
+    ctx.fill();
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(465, 500);
+    ctx.lineTo(465, 250);
+    ctx.moveTo(535, 500);
+    ctx.lineTo(535, 250);
+    ctx.fillStyle = '#396693';
+    ctx.fillRect(465, 250, 70, 250);
+    ctx.moveTo(465, 500);
+    ctx.quadraticCurveTo(500, 550, 535, 500);
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+
+    ctx.save();
+    ctx.scale(1, 0.5);
+    ctx.beginPath();
+    ctx.arc(500, 500, 35, 0, 2 * Math.PI);
+    ctx.strokeStyle = '#000';
+    ctx.stroke();
+    ctx.fillStyle = '#396693';
+    ctx.fill();
 })();
---
+++ Canvas/02. BouncingBall/bouncing-ball.js
@@ -0,0 +1,97 @@
+﻿(function () {
+    "use strict";
+
+    var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+        direction = {
+            x: "right",
+            y: "down",
+        },
+        directions = {
+            "left": -1.2,
+            "right": +1.5,
+            "up": -0.9,
+            "down": +0.5
+        };
+
+    function getRandomValue(min, max) {
+        return (Math.random() * (max - min) + min) | 0;
+    }
+
+    function getRandomColor() {
+        var red = getRandomValue(0, 255),
+            green = getRandomValue(0, 255),
+            blue = getRandomValue(0, 255);
+
+        return "rgb(" + red + "," + green + "," + blue + ")";
+    }
+
+    function Ball(x, y, radius, speed, direction) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = speed;
+        this.direction = direction;
+        this.fillColor = getRandomColor();
+        this.strokeColor = getRandomColor();
+
+        this.draw = function (ctx) {
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            ctx.fillStyle = this.fillColor;
+            ctx.strokeStyle = this.strokeColor;
+            ctx.fill();
+            ctx.stroke();
+        };
+
+        this.move = function () {
+            this.x += this.speed * directions[this.direction.x];
+            this.y += this.speed * directions[this.direction.y];
+        };
+
+        this.bounce = function (limitX, limitY) {
+            if (this.x < this.radius) {
+                this.direction.x = "right";
+            }
+
+            if (this.x > limitX - this.radius) {
+                this.direction.x = "left";
+            }
+
+            if (this.y < this.radius) {
+                this.direction.y = "down";
+            }
+
+            if (this.y > limitY - this.radius) {
+                this.direction.y = "up";
+            }
+        }; 
+    }
+
+    function getRandomBall() {
+        var x = getRandomValue(0, ctx.canvas.width),
+            y = getRandomValue(0, ctx.canvas.height),
+            directionX = (getRandomValue(0, 2) % 2 === 0) ? "left" : "right",
+            directionY = (getRandomValue(0, 2) % 2 === 0) ? "up" : "down",
+            ball = new Ball(x, y, 20, 5, {
+                x: directionX,
+                y: directionY
+            });
+
+        return ball;
+    }
+
+    var myBall = getRandomBall();
+
+    function animationFrame() {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+        myBall.bounce(ctx.canvas.width, ctx.canvas.height);
+        myBall.move();
+        myBall.draw(ctx);
+
+        requestAnimationFrame(animationFrame);
+    }
+
+    animationFrame();
+})();
