fc3673334883c667d2f79ae2956fd3073f614ca1
Dimitar Mitev <dnmitev@gmail.com>
2014-06-05 17:09:19 +0300

    raphaeljs hw started

---
+++ Raphael/01. Logos/logos.js
@@ -0,0 +1,26 @@
+﻿/// <reference path="../Scripts/_references.js" />
+(function () {
+    "use strict";
+
+    var paper = new Raphael(10, 10, 960, 500);
+
+    var text = paper.text(350, 100, "Telerik®");
+    text.attr({
+        "font-weight": "bold",
+        "font-size": 100,
+        "font-family": "Calibri, Consolas, Arial"
+    });
+
+    var slogan = paper.text(340, 150, "Develop experiences");
+    slogan.attr({
+        "font-size": 35,
+        "font-family": "Calibri, Consolas, Arial"
+    });
+
+    var telerikLogo = paper.path("M40 80 L75 50 L125 125 L100 155 L75 125 L125 50 L160 80")
+    telerikLogo.attr({
+        "stroke-width": 10,
+        stroke: "#C1D832"
+    });
+
+})();
---
+++ Raphael/Scripts/_references.js
@@ -0,0 +1,3 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../bower_components/raphael/raphael.js" />
+/// <reference path="../01. Logos/logos.js" />
