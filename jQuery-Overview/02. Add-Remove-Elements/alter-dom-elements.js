c5b02134f5bb2e2d6e4b541a7b91529c9eab1c31
Dimitar Mitev <dnmitev@gmail.com>
2014-06-12 17:00:17 +0300

    1st three task done
    there is still to be done. HW is not finished yet

---
+++ jQuery-Overview/02. Add-Remove-Elements/alter-dom-elements.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="../Scripts/_references.js" />
+(function () {
+    "use strict";
+
+    var $root = $('#list'),
+        $addBeforeButton = $('#add-before-btn'),
+        $addAfterButton = $('#add-after-btn');
+
+    $addAfterButton.on('click', function () {
+        $root.append('<li>Item after</li>');
+    });
+
+    $addBeforeButton.on('click', function () {
+        $root.prepend('<li>Item before</li>');
+    });
+}());
---
+++ jQuery-Overview/03. Students-Table/students-table.js
@@ -0,0 +1,49 @@
+﻿/// <reference path="../Scripts/_references.js" />
+(function () {
+    "use strict";
+
+    var students = [{
+        firstName: 'Pepi',
+        lastName: 'Peichev',
+        grade: 12
+    }, {
+        firstName: 'Pesho',
+        lastName: 'Goshev',
+        grade: 12
+    }, {
+        firstName: 'Gosho',
+        lastName: 'Peshev',
+        grade: 11
+    }, {
+        firstName: 'Gubi',
+        lastName: 'Buklik',
+        grade: 2
+    }, {
+        firstName: 'Spindi',
+        lastName: 'Spinderov',
+        grade: 3
+    }, {
+        firstName: 'Jmirgoleta',
+        lastName: 'Lozanova',
+        grade: 1
+    }],
+        theadString = '<thead>' +
+                            '<tr>' +
+                                '<th>First Name</th>' +
+                                '<th>Last Name</th>' +
+                                '<th>Grade</th>' +
+                            '</tr>' +
+                      '</thead>',
+        tbodyString = '<tbody>';
+    //tbodyString = '<tbody><tr><td>.....</td></tr></tbody>';
+
+    for (var i = 0, len = students.length; i < len; i += 1) {
+        tbodyString += '<tr>' +
+                            '<td>' + students[i].firstName + '</td>' +
+                            '<td>' + students[i].lastName + '</td>' +
+                            '<td>' + students[i].grade + '</td>' +
+                        '</tr>';
+    }
+
+    $('#wrapper').append('<table id="students">' + theadString + tbodyString + '</table>');
+}());
